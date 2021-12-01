974bea2411e7ea869515a6e71705b13745603238
XenoX <gauthier.burgeat@gmail.com>
2021-12-01 20:32:23 +0100

    :tada: Initial commit

---
+++ src/2021/1_1.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\ErrorHandler\Debug;
+
+require_once __DIR__.'/../../vendor/autoload.php';
+
+Debug::enable();
+
+$numbers = file_get_contents(__DIR__ . '/../../data/2021/1.txt');
+$numbers = explode("\n", $numbers);
+$numberOfIncrements = 0;
+
+for ($i = 0; $i < count($numbers); $i++) {
+    if (!array_key_exists(($i - 1), $numbers) || $numbers[$i - 1] > $numbers[$i]) {
+        continue;
+    }
+
+    ++$numberOfIncrements;
+}
+
+echo $numberOfIncrements."\n";
---
+++ src/2021/1_2.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\ErrorHandler\Debug;
+
+require_once __DIR__.'/../../vendor/autoload.php';
+
+Debug::enable();
+
+$numbers = file_get_contents(__DIR__ . '/../../data/2021/1.txt');
+$numbers = explode("\n", $numbers);
+$previousSum = null;
+$numberOfIncrements = 0;
+
+for ($i = 0; $i < count($numbers); $i++) {
+    if (!array_key_exists(($i + 2), $numbers)) {
+        continue;
+    }
+
+    $currentSum = $numbers[$i] + $numbers[$i + 1] + $numbers[$i + 2];
+
+    if (null === $previousSum || $previousSum >= $currentSum) {
+        $previousSum = $currentSum;
+
+        continue;
+    }
+
+    $previousSum = $currentSum;
+    ++$numberOfIncrements;
+}
+
+echo $numberOfIncrements."\n";
